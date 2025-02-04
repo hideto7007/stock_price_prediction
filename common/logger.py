f73abab19f11d1689e2490b45e631c1544e1083f
hideto <ma_kux@icloud.com>
2025-02-04 20:46:50 +0900

    logger修正

--- common/logger.py
+++ common/logger.py
@@ -1,6 +1,10 @@
 import logging
+from fastapi import Request
+import json
 
 from datetime import datetime, timedelta, timezone
+import os
+import traceback
 
 
 class ISOTimeFormatter(logging.Formatter):
@@ -12,39 +16,96 @@ class ISOTimeFormatter(logging.Formatter):
         return s
 
 
-file_name = './Users/hideto/money_management/stock_price_prediction/logger.log'
+file_name = './logger.log'
 
 
-logger = logging.getLogger()
-fmt = ISOTimeFormatter(
-    '%(asctime)s - %(levelname)s - %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S'
-)
+class SingletonLogger:
+    _instance = None
 
-sh = logging.StreamHandler()
-sh.setFormatter(fmt)
-logger.addHandler(sh)
+    def __new__(cls, log_file=file_name):
+        if cls._instance is None:
+            cls._instance = super(SingletonLogger, cls).__new__(cls)
+            cls._instance._initialize(log_file)
+        return cls._instance
 
-logger.setLevel(logging.INFO)
-logging.basicConfig(
-    filename=file_name,
-    level=logging.DEBUG
-)
+    def _initialize(self, log_file):
+        """ロガーの初期化処理"""
+        self.logger = logging.getLogger("app_logger")
+        if self.logger.hasHandlers():
+            return  # すでにハンドラが設定されていたら何もしない
+
+        self.logger.setLevel(logging.DEBUG)
+
+        # ログファイル存在チェック
+        if not os.path.isfile(log_file):
+            with open(log_file, "w"):
+                pass
+
+        # ファイルハンドラ
+        file_handler = logging.FileHandler(
+            log_file, mode='a', encoding='utf-8')
+        file_handler.setLevel(logging.DEBUG)
+        fmt = ISOTimeFormatter(
+            '%(asctime)s - %(levelname)s - %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S'
+        )
+        file_handler.setFormatter(fmt)
+        self.logger.addHandler(file_handler)
+
+    def get_logger(self):
+        return self.logger
 
 
 class Logger:
-    @classmethod
-    def info(cls, data):
-        return logger.info(data)
+    """ロギングヘルパークラス"""
+    @staticmethod
+    async def error(req: Request, exc: Exception):
+        """ERROR レベルのログを出力"""
+        logger = SingletonLogger().get_logger()
 
-    @classmethod
-    def debug(cls, data):
-        return logger.debug(data)
+        try:
+            body = await req.json()
+        except Exception:
+            body = await req.body()
 
-    @classmethod
-    def warning(cls, data):
-        return logger.warning(data)
+        log_data = {
+            "request_id": req.state.request_id,
+            "method": req.method,
+            "params": dict(req.query_params),
+            "body": body,
+            "url": str(req.url),
+            "error": str(exc)
+        }
 
-    @classmethod
-    def error(cls, data):
-        return logger.error(data)
+        logger.error(json.dumps(log_data, ensure_ascii=False))  # JSON形式でログを出力
+        traceback.print_exc()  # 例外の詳細なトレースバックを出力
+
+    @staticmethod
+    async def info(req: Request, message: str):
+        """INFO レベルのログを出力"""
+        logger = SingletonLogger().get_logger()
+
+        log_data = {
+            "request_id": req.state.request_id,
+            "method": req.method,
+            "params": dict(req.query_params),
+            "url": str(req.url),
+            "message": message
+        }
+
+        logger.info(json.dumps(log_data, ensure_ascii=False))
+
+    @staticmethod
+    async def debug(req: Request, message: str):
+        """DEBUG レベルのログを出力"""
+        logger = SingletonLogger().get_logger()
+
+        log_data = {
+            "request_id": req.state.request_id,
+            "method": req.method,
+            "params": dict(req.query_params),
+            "url": str(req.url),
+            "message": message
+        }
+
+        logger.debug(json.dumps(log_data, ensure_ascii=False))
